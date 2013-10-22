3752f6be36ef5634701b4932feb77fb7e885a3cf
kamilw7 <kamilw7@gmail.com>
2013-10-22 23:37:24 +0200

    topten itd

--- content/all.php
+++ content/all.php
@@ -11,9 +11,15 @@ require_once('classes/db/db.php');
 require_once("classes/cutter/cut.php");
 require_once('includes/show.php');
 
+if (isset($_GET["filter"])){
+switch ($_GET["filter"])
+{
+
+//--------------------------------------------------------------------//
+case "latest":
 $baza = new DBconn;
 $baza->connect();
-$thb = $baza->getimgs(9,"`added`");
+$thb = $baza->getimgs(50,"`added`");
 
 echo '<div id="thumbnails">';
 
@@ -29,8 +35,78 @@ echo '</font></td></tr></table>';
 echo '</a>';
 echo '</div>';
 }
-
 echo '</div>';
+break;
+//--------------------------------------------------------------------//
+case "topten":
+$baza = new DBconn;
+$baza->connect();
+$thb = $baza->getimgs(10,"`rate`");
+
+echo '<div id="thumbnails">';
+
+foreach ($thb as $thumb){
+echo '<div id="thumb">';
+echo '<a href="./?page=show&fileid='.$thumb['fileid'].'">';
+echo '<table align="center"><tr><td>';
+$img = showcatthumb($thumb['fileid']);
+echo $img;
+echo '</td></tr><tr><td height="25" valign="middle"><font size="2">';
+echo cS($thumb['name'], 13);
+echo '</font></td></tr></table>';
+echo '</a>';
+echo '</div>';
+}
+echo '</div>';
+break;
+//--------------------------------------------------------------------//
+default:
+$baza = new DBconn;
+$baza->connect();
+$thb = $baza->getimgs(100,"RAND()");
+
+echo '<div id="thumbnails">';
+
+foreach ($thb as $thumb){
+echo '<div id="thumb">';
+echo '<a href="./?page=show&fileid='.$thumb['fileid'].'">';
+echo '<table align="center"><tr><td>';
+$img = showcatthumb($thumb['fileid']);
+echo $img;
+echo '</td></tr><tr><td height="25" valign="middle"><font size="2">';
+echo cS($thumb['name'], 13);
+echo '</font></td></tr></table>';
+echo '</a>';
+echo '</div>';
+}
+echo '</div>';
+break;
+//--------------------------------------------------------------------//
+}
+} else {
+
+$baza = new DBconn;
+$baza->connect();
+$thb = $baza->getimgs(100,"RAND()");
+
+echo '<div id="thumbnails">';
+
+foreach ($thb as $thumb){
+echo '<div id="thumb">';
+echo '<a href="./?page=show&fileid='.$thumb['fileid'].'">';
+echo '<table align="center"><tr><td>';
+$img = showcatthumb($thumb['fileid']);
+echo $img;
+echo '</td></tr><tr><td height="25" valign="middle"><font size="2">';
+echo cS($thumb['name'], 13);
+echo '</font></td></tr></table>';
+echo '</a>';
+echo '</div>';
+}
+echo '</div>';
+
+}
+
 ?>
 
 
--- includes/menu.php
+++ includes/menu.php
@@ -10,7 +10,7 @@
 <a href="./?page=add"> Podłóż komuś świnię&nbsp&nbsp</a>
 |<a href="./?page=all">&nbsp&nbspPrzeglądaj wszystkie świnie&nbsp&nbsp</a>
 |<a href="./?page=all&filter=latest">&nbsp&nbspNajnowsze&nbsp&nbsp</a>
-|<a href="./?page=all&filtertopten">&nbsp&nbspTOP10&nbsp&nbsp</a>
+|<a href="./?page=all&filter=topten">&nbsp&nbspTOP10&nbsp&nbsp</a>
 |<a href="./?page=mission">&nbsp&nbspCel seriwsu&nbsp&nbsp</a>
 |<a href="./?page=policies">&nbsp&nbspRegulamin&nbsp&nbsp</a>
 |<a href="./?page=contact">&nbsp&nbspKontakt&nbsp&nbsp</a>
