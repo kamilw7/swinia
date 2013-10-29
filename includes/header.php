c72ae549bc9dc0014b86e13ae8162ea5d6be3fef
kamilw7 <kamilw7@gmail.com>
2013-10-29 22:05:45 +0100

    pl-PL, 3 kolumny

--- includes/header.php
+++ includes/header.php
@@ -3,11 +3,11 @@ session_start();
 ?>
 
 <!doctype html>
-<!--[if !IE]>      <html class="no-js non-ie" lang="en-US"> <![endif]-->
-<!--[if IE 7 ]>    <html class="no-js ie7" lang="en-US"> <![endif]-->
-<!--[if IE 8 ]>    <html class="no-js ie8" lang="en-US"> <![endif]-->
-<!--[if IE 9 ]>    <html class="no-js ie9" lang="en-US"> <![endif]-->
-<!--[if gt IE 9]><!--> <html class="no-js" lang="en-US"> <!--<![endif]-->
+<!--[if !IE]>      <html class="no-js non-ie" lang="pl-PL"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="pl-PL"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="pl-PL"> <![endif]-->
+<!--[if IE 9 ]>    <html class="no-js ie9" lang="pl-PL"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="pl-PL"> <!--<![endif]-->
 <head>
 
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
